381578a42e58b69a45d9f32de2ac1cc06168c04a
saipr0 <saiprabhat.05@gmail.com>
2025-09-26 22:28:01 +0530

    social links

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,5 +1,13 @@
 import { initAboutTheme } from '../theme.js';
 import { makeLinksExternal } from '../utils.js';
+import { icons } from '../svg.js';
+
+const SOCIAL_LINKS = {
+  github: 'https://github.com/saipr0',
+  twitter: 'https://twitter.com/saipr_',
+  linkedin: 'https://linkedin.com/in/sai-prabhat',
+  email: 'saiprabhat@pm.me'
+};
 
 export async function loadAbout() {
   const r = await fetch(`./content/about.md`);
@@ -9,8 +17,26 @@ export async function loadAbout() {
   document.getElementById('about-content').innerHTML = `
     <img id="about-banner" class="def-img" src="" alt="">
     <div class="post-body">${htmlContent}</div>
+    ${socials}
   `;
 
   initAboutTheme();
   makeLinksExternal('#about-content');
 }
+
+const socials = `
+<div class="socials-links">
+	<a class="social-link" href="${SOCIAL_LINKS.github}" target="_blank" rel="noopener noreferrer">
+		${icons.github}
+	</a>
+	<a class="social-link" href="mailto:${SOCIAL_LINKS.email}">
+		${icons.email}
+	</a>
+	<a class="social-link" href="${SOCIAL_LINKS.twitter}" target="_blank" rel="noopener noreferrer">
+		${icons.twitter}
+	</a>
+	<a class="social-link" href="${SOCIAL_LINKS.linkedin}" target="_blank" rel="noopener noreferrer">
+		${icons.linkedin}
+	</a>
+</div>
+`;
